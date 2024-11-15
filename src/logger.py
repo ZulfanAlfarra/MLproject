4278791259c19f3125a7b6b9757a63dffb39dc5e
ZulfanAlfarra <zulfansyahidanalfarra.91@gmail.com>
2024-11-15 10:31:09 +0700

    modified logging file

--- src/logger.py
+++ src/logger.py
@@ -6,14 +6,22 @@ from exception import CustomException
 import sys
 
 LOG_FILE = f"{datetime.now().strftime('%m_%d_%Y_%H_%M_%S')}.log"
-logs_path = os.path.join(os.getcwd(), "logs", LOG_FILE)
+logs_path = os.path.join(os.getcwd(), "logs")
 os.makedirs(logs_path, exist_ok=True)
 
-LOG_FILE_PATH = os.path.join(logs_path, LOG_FILE)
+LOG_FILE_PATH = os.path.join(logs_path,  LOG_FILE)
 
 logging.basicConfig(
     filename=LOG_FILE_PATH,
     format="[ %(asctime)s ] %(lineno)d %(name)s - %(levelname)s %(message)s",
     level=logging.INFO
     )
+
+if __name__ == "__main__":
+    logging.info("Logging has started")
+    try:
+        a = 1/0
+    except Exception as e:
+        logging.info("Divide by Zero")
+        raise CustomException(e, sys)
 
